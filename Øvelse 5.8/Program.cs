2656e7ce7ce16b5cf7dd00640803706331f04e00
Skole <Skole@DESKTOP-87FGBUN>
2024-04-18 08:24:03 +0200

    Done

--- Øvelse 5.8/Program.cs
+++ Øvelse 5.8/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+for (int linje = 1; linje < 11; linje++)
+{
+    for (int tal = linje; tal < linje * 10 + 1; tal = tal + linje)
+    {
+        Console.Write($"{tal} ");
+    }
+    Console.WriteLine();
+}
